4c1fe483d5a7d48452dab707847e611fa80c0f44
YumMinSeok <minseok@sangilmedia.hs.kr>
2023-08-16 23:36:30 +0900

    08/16

--- 01.py
+++ 01.py
@@ -1,1 +1,11 @@
-print("hello")
+from eunjeon import Mecab
+
+mecab = Mecab()
+
+words = ['유튜브','는','수행','도','단계','해결','업데이트']
+
+for word2 in words:
+        if len(word2) < 2:
+            words.remove(word2)
+            
+print(words)
